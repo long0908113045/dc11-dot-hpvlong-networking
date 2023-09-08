6e80a45c29b05a17a95f1cdf2282df60995a4602
Long Ho <long0908113045@gmail.com>
2023-09-08 09:32:42 +0000

    update

--- main.tf
+++ main.tf
@@ -1,24 +1,15 @@
 terraform {
   backend "s3" {
     bucket                  = "dc11-dot-hpvlong-terraform-state"
-    key                     = "terraform-state"
+    dynamodb_table          = "dc11-dot-hpvlong-terraform-state-lock"
+    key                     = "terraform.tfstate"
     region                  = "ap-southeast-1"
     shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
-  region                  = "ap-southeast-1"
+  region                  = "${terraform.workspace == "test" ? "ap-southeast-1" : "ap-southeast-2"}"
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                 = "default"
-}
-
-
-resource "aws_s3_bucket" "New_bucket" {
-  bucket = "dc11-dot-hpvlong-terraform-state2"
-  acl    = "private"
-
-  tags = {
-    Name = "hpvlong"
-  }
+  profile                 = "${terraform.workspace == "test" ? "test" : "stg"}"
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+	type = string
+    default = "ap-southeast-1"
+    description = "the region of aws"
+}
+
+variable "profile" {
+	type = string
+    default = "stg"
+    description = "the profile of aws"
+}
