1e06edd014a51a1f92863caf418247450bd5a51e
hpvlong <long0908113045@gmail.com>
2023-09-12 08:58:11 +0000

    setup project

--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
 terraform {
   backend "s3" {
-    bucket                  = "dc11-dot-hpvlong-terraform-state"
-    dynamodb_table          = "dc11-dot-hpvlong-terraform-state-lock"
+    bucket                  = "dc11-dot-hpvlong-networking-terraform-state"
+    dynamodb_table          = "dc11-dot-hpvlong-networking-terraform-state-lock"
     key                     = "terraform.tfstate"
     region                  = "ap-southeast-1"
     shared_credentials_file = "~/.aws/credentials"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.16.2"
+    }
+  }
+  required_version = ">= 1.5.7"
 }
 
-provider "aws" {
-  region                  = "${terraform.workspace == "test" ? "ap-southeast-1" : "ap-southeast-2"}"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                 = "${terraform.workspace == "test" ? "test" : "stg"}"
+locals {
+  config_file_name      = "${terraform.workspace}.tfvars"
+  full_config_file_path = "variables/${local.config_file_name}"
+  vars                  = yamldecode(file(local.full_config_file_path))
 }
+
+
+provider "aws" {
+  region                  = local.vars.region
+  profile                 = local.vars.profile
+}
+
